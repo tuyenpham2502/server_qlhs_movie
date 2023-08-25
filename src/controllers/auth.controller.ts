29f6aa83530663c6431babf29c7298caafe43de5
Tuyên Phạm <DT050236@actvn.edu.vn>
2023-08-25 14:07:14 +0700

    update

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,112 @@
+import config from 'config';
+import { CookieOptions, NextFunction, Request, Response } from 'express';
+import { CreateUserInput, LoginUserInput } from '../schema/user.schema';
+import { createUser, findUser, signToken } from '../services/user.service';
+import AppError from '../utils/appError';
+
+// Exclude this fields from the response
+export const excludedFields = ['password'];
+
+// Cookie options
+
+const cookieOptions: CookieOptions = {
+  httpOnly : true,
+  sameSite: 'lax',
+  secure: true,
+};
+
+const accessTokenCookieOptions: CookieOptions = {
+  ... cookieOptions,
+  expires: new Date(
+    Date.now() + config.get<number>('accessTokenExpiresIn') * 60 * 1000
+  ),
+  maxAge: config.get<number>('accessTokenExpiresIn') * 60 * 1000,
+};
+
+const refreshTokenCookieOptions: CookieOptions = {
+  ... cookieOptions,
+  expires: new Date(
+    Date.now() + config.get<number>('refreshTokenExpiresIn') * 24 * 60 * 60 * 1000
+  ),
+  maxAge: config.get<number>('refreshTokenExpiresIn') * 24 * 60 * 60 * 1000,
+};
+
+
+
+// Only set secure to true in production
+if (process.env.NODE_ENV === 'production')
+  cookieOptions.secure = true;
+
+
+
+export const registerHandler = async (
+  req: Request<{}, {}, CreateUserInput>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await createUser({
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (err: any) {
+    if (err.code === 11000) {
+      return res.status(409).json({
+        status: 'fail',
+        message: 'Email already exist',
+      });
+    }
+    next(err);
+  }
+};
+
+export const loginHandler = async (
+  req: Request<{}, {}, LoginUserInput>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Get the user from the collection
+    const user = await findUser({ email: req.body.email });
+
+    // Check if user exist and password is correct
+    if (
+      !user ||
+      !(await user.comparePasswords(user.password, req.body.password))
+    ) {
+      return next(new AppError('Invalid email or password', 401));
+    }
+
+    // Create an Access Token, refresh token and send it to the client
+    const { access_token, refresh_token } = await signToken(user);
+
+    
+    res.cookie('accessToken', access_token, accessTokenCookieOptions);
+
+    // Send Refresh Token in Cookie
+    res.cookie('refreshToken', refresh_token, refreshTokenCookieOptions);
+
+    res.cookie('logged_in', true, {
+      ...accessTokenCookieOptions,
+      httpOnly: false,
+    });
+
+    // Send Access Token
+    res.status(200).json({
+      status: 'success',
+      access_token,
+      refresh_token,
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
---
+++ src/utils/connectDB.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+const dbUrl = `mongodb+srv://${config.get('dbName')}:${config.get('dbPass')}@qlhsmoviedev.i0mbtte.mongodb.net/?retryWrites=true&w=majority`;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbUrl);
+    console.log('Database connected...');
+  } catch (error: any) {
+    console.log(error.message);
+    setTimeout(connectDB, 5000);
+  }
+};
+
+export default connectDB;
+
