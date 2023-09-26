4a5fdedf363740942bcacf624a61308b5c488241
Tuyên Phạm <DT050236@actvn.edu.vn>
2023-09-26 20:26:02 +0700

    fix

--- src/utils/connectRedis.ts
+++ src/utils/connectRedis.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'redis';
 
-const redisUrl = `redis://redis:6379`;
+const redisUrl = `redis://localhost:6379`;
 const   redisClient = createClient({
   url: redisUrl,
 });
