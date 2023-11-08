cced1bc4d7d6a3b0f2b65522a7cf4aa08c216710
Tuyên Phạm <DT050236@actvn.edu.vn>
2023-11-08 14:26:47 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,5 @@ RUN npm install -g pm2
 
 EXPOSE 8080
 
-CMD ["pm2-runtime", "ecosystem.config.js", "--env", "production"]
+# CMD ["pm2-runtime", "ecosystem.config.js", "--env", "production"]
 
---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+while getopts "e:v:h" opt
+do
+  # shellcheck disable=SC2220
+  case "$opt" in
+    e) env="$OPTARG" ;;
+    v) version="$OPTARG" ;;
+  esac
+done
+
+while IFS= read -r line; do
+  export "$line"
+done < src/.env
+
+# Export to system env for docker compose 
+export version=$env-v$version
+case "$env" in
+  development)
+    source src/.env
+    export env=dev
+    export COMPOSE_PROJECT_NAME="qlhs-dev-server-services" 
+    export command='yarn prod'
+    ;;
+#    uat)
+#       export env=uat
+#       export COMPOSE_PROJECT_NAME="qs-prod-biz-services"
+#       export command='yarn start:uat'
+#       source ~/.zshrc
+#       ;;
+#   production)
+#     export env=prod
+#     export COMPOSE_PROJECT_NAME="qs-prod-biz-services"
+#     export command='yarn start:prod'
+#     source ~/.zshrc
+#     ;;
+#   uat)
+#     export env=UAT
+#     export COMPOSE_PROJECT_NAME="qs-uat-biz-services"
+#     export command='yarn start'
+#     source ~/.zshrc
+#     ;;
+esac
+
+echo "Building version [$version] service on {$env} environment"
+docker-compose -f docker-compose.$env.yml down
+docker-compose -f docker-compose.$env.yml config
+docker-compose -f docker-compose.$env.yml build --no-cache --progress=plain
+docker-compose -f docker-compose.$env.yml up -d
